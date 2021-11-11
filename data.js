65949ee2e2768270e0dbf5db8158b5141b3c28c9
Thomas Chen <thomas.arsenal.chen@gmail.com>
2021-11-11 00:19:49 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,11 @@
+var detailedBandObject = {
+  'australia' : {
+    'buildings' : ['Gold Coast', 'Melbourne', 'Melbourne Two'],
+
+
+  },
+  'korea': {
+    'buildings' : ['Seoul 1', 'Seoul 2', 'Busan 1', 'Busan 2', "Incheon"],
+
+  }
+};
---
+++ script.js
@@ -0,0 +1,47 @@
+//basic jquery
+$(document).ready(() => {
+  // keep the line above
+
+  //hide
+
+  $(".bottom").hide();
+
+//slide
+  $(".clicker").on("click", event => {
+    $(event.currentTarget)
+      .closest("section")
+      .siblings()
+      .find(".bottom")
+      .slideUp();
+
+    $(event.currentTarget)
+      .parent()
+      .next()
+      .slideToggle();
+  });
+
+
+});
+
+
+//trying the app structure and loading data
+const app = {
+  data: detailedBandObject, //this comes from my data.js files
+
+    initialize: () => {
+    $('.compareSky').click(function () {
+      app.compareSky();
+    })
+  },
+
+
+    compareSky: () => {
+    const australiaBuildings = app.data.australia.buildings;
+    const koreaBuildings = app.data.korea.buildings;
+
+    $('.skyComparison').text(
+      `${australiaBuildings.length > koreaBuildings.length ? 'Australia' : 'Korea'}
+    had more skyscrapers (300m+)`);
+  }
+
+}
